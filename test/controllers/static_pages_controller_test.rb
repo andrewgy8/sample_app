c0c2fe5b440dd228393e1618f96ea21232507593
andrewgy8 <andrewgy8@gmail.com>
2016-05-23 10:15:53 +0200

    Eliminate repition in test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,24 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "| Andrew's Sample Site"
+  end 
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Andrew's Sample Site"
+    assert_select "title", "Home #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Andrew's Sample Site"
+    assert_select "title", "Help #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Andrew's Sample Site"
+    assert_select "title", "About #{@base_title}"
   end
 end
