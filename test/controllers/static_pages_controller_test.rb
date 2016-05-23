ad23e89d9e01ee369c2dadbcd5671cb7ee0bbedb
andrewgy8 <andrewgy8@gmail.com>
2016-05-23 11:03:09 +0200

    Finished full title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Andrew's Sample Site"
+    assert_select "title", "Andrew's Sample Site"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Andrew's Sample Site"
+    assert_select "title", "Andrew's Sample Site"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Andrew's Sample Site"
+    assert_select "title", "Andrew's Sample Site"
   end
 end
