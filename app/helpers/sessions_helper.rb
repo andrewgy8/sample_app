3da5b1d93fe698bfc00b8d89eaec6abf8e57a486
Andrew Graham-Yooll <andrewgy8@gmail.com>
2016-07-08 15:04:36 +0200

    initial on the new comp

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
   def log_in(user)
-    #creates a temporary cookie that is encrypeted.
+    #creates a temporary cookie that is encrypeted using the seesion method.
     session[:user_id] = user_id
   end
 end
